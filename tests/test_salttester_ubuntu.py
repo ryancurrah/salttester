4ecfc8f86699e0a9e10fa4b32955cfed05fd2997
Ryan Currah <ryan@currah.ca>
2016-01-10 14:22:02 -0500

    Added new test for serverspec and changed version

--- tests/test_salttester_ubuntu.py
+++ tests/test_salttester_ubuntu.py
@@ -1,5 +1,5 @@
 """
-Salttester tests for ubuntu AWS deployer
+Salttester tests for Ubuntu images
 """
 import logging
 import os
@@ -32,8 +32,12 @@ PYTEST_ARGS = ['-b', BUILD_ID, '-o', 'ubuntu', 'remote', '--port', '22', '--user
                '--key-filename', '/srv/test/test.pem', 'pytest', '--pytest-test-dir',
                '/srv/salt/tests/pytest']
 
-HIGHSTATE_ARGS = ['-b', BUILD_ID, '-o', 'ubuntu', 'remote', '--port', '22', '--username', 'ubuntu',
-                  '--key-filename', '/srv/test/test.pem', 'highstate']
+HIGHSTATE_ARGS = ['-b', BUILD_ID, '-o', 'ubuntu', 'remote', '--port', '22', '--username',
+                  'ubuntu', '--key-filename', '/srv/test/test.pem', 'highstate']
+
+SERVERSPEC_ARGS = ['-b', BUILD_ID, '-o', 'ubuntu', 'remote', '--port', '22', '--username',
+                   'ubuntu', '--key-filename', '/srv/test/test.pem', 'serverspec',
+                   '--serverspec-test-dir', '/srv/salt/tests/serverspec/linux']
 
 TERMINATE_ARGS = ['-b', BUILD_ID, '-o', 'ubuntu', 'terminate']
 
@@ -47,14 +51,21 @@ def ec2_instance(request):
     :return: ec2 instance deploy exit code
     """
     parser = salttester.__main__.create_parser()
-    # pylint: disable=W0612
     exit_code, stdout, stderr = salttester.deploy.run(parser.parse_args(DEPLOY_ARGS), logging)
 
     def fin():
+        """
+        Teardown ec2 instance once module testing done
+
+        :return: None
+        """
         parser = salttester.__main__.create_parser()
         salttester.terminate.run(parser.parse_args(TERMINATE_ARGS), logging)
     request.addfinalizer(fin)
-    return exit_code
+
+    if exit_code != 0:
+        raise Exception('Unable to initiate an ec2 instance fixture!')
+    return exit_code, stdout, stderr
 
 
 def test_build_data_db_file_name():
@@ -85,12 +96,11 @@ def test_userdata():
 
 def test_deploy_ubuntu(ec2_instance):
     """
-    Tests that ec2 instance was deployed, there is an instance iid and public ip
+    Tests that there is an instance id and public ip
     Test that there is an instance with the status running
 
     :param ec2_instance: Pytest fixture
     """
-    assert ec2_instance == 0
     instance_id = salttester.build_data.read(BUILD_ID, 'instance', 'id')
     public_ip_address = salttester.build_data.read(BUILD_ID, 'instance', 'public_ip_address')
     assert isinstance(instance_id, str)
@@ -102,12 +112,10 @@ def test_deploy_ubuntu(ec2_instance):
 
 def test_good_remote_cmd(ec2_instance):
     """
-    Tests that ec2 instance was deployed
     Test that remote command execution works and proper result code returned
 
     :param ec2_instance: Pytest fixture
     """
-    assert ec2_instance == 0
     parser = salttester.__main__.create_parser()
     # pylint: disable=W0612
     exit_code, stdout, stderr = salttester.cmd.run(parser.parse_args(GOOD_COMMAND_ARGS), logging)
@@ -122,7 +130,6 @@ def test_bad_remote_cmd(ec2_instance):
 
     :param ec2_instance: Pytest fixture
     """
-    assert ec2_instance == 0
     parser = salttester.__main__.create_parser()
     # pylint: disable=W0612
     exit_code, stdout, stderr = salttester.cmd.run(parser.parse_args(BAD_COMMAND_ARGS), logging)
@@ -132,12 +139,10 @@ def test_bad_remote_cmd(ec2_instance):
 
 def test_get_file_sftp(ec2_instance):
     """
-    Tests that ec2 instance was deployed
     Test that remote file download works
 
     :param ec2_instance: Pytest fixture
     """
-    assert ec2_instance == 0
     parser = salttester.__main__.create_parser()
     # pylint: disable=W0612
     exit_code, stdout, stderr = salttester.get_file.run(parser.parse_args(GET_FILE_ARGS), logging)
@@ -145,31 +150,47 @@ def test_get_file_sftp(ec2_instance):
     assert os.path.isfile('testfile.txt')
 
 
+@pytest.mark.run(order=1)
 def test_pystest_tester(ec2_instance):
     """
-    Tests that ec2 instance was deployed
-    Test that pytest runs successfully
+    Test that pytest runs successfully and downloads a pytest.xml file
 
     :param ec2_instance: Pytest fixture
     """
-    assert ec2_instance == 0
     parser = salttester.__main__.create_parser()
     # pylint: disable=W0612
     exit_code, stdout, stderr = salttester.pytest.run(parser.parse_args(PYTEST_ARGS), logging)
     assert exit_code == 0
     assert os.path.isfile('pytest.xml')
+    assert os.stat('pytest.xml').st_size > 0
 
 
+@pytest.mark.run(order=2)
 def test_highstate_tester(ec2_instance):
     """
-    Tests that ec2 instance was deployed
-    Test that highstate runs successfully
+    Test that highstate runs successfully and downloads a highstate.xml file
 
     :param ec2_instance: Pytest fixture
     """
-    assert ec2_instance == 0
     parser = salttester.__main__.create_parser()
     # pylint: disable=W0612
     exit_code, stdout, stderr = salttester.highstate.run(parser.parse_args(HIGHSTATE_ARGS), logging)
     assert exit_code == 0
     assert os.path.isfile('highstate.xml')
+    assert os.stat('highstate.xml').st_size > 0
+
+
+@pytest.mark.run(order=3)
+def test_serverspec_tester(ec2_instance):
+    """
+    Test that serverspec runs successfully and downloads a serverspec.xml file
+
+    :param ec2_instance: Pytest fixture
+    """
+    parser = salttester.__main__.create_parser()
+    # pylint: disable=W0612
+    exit_code, stdout, stderr = salttester.serverspec.run(parser.parse_args(SERVERSPEC_ARGS),
+                                                          logging)
+    assert exit_code == 0
+    assert os.path.isfile('serverspec.xml')
+    assert os.stat('serverspec.xml').st_size > 0
